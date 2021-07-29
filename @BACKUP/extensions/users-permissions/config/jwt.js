c6df3446158364e7da61b1269e4a3456cf8a32da
MarcusMystrader - pessoalComputer <mystrader@gmail.com>
2021-07-29 20:28:01 -0300

    update: exemplo adicionando um botao no feature item

---
+++ @BACKUP/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '49201149-57be-4072-a577-72c39b839803'
+};
---
+++ config/database.js
@@ -0,0 +1,21 @@
+module.exports = ({ env }) => ({
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'mongoose',
+      settings: {
+        host: env('DATABASE_HOST', '127.0.0.1'),
+        srv: env.bool('DATABASE_SRV', false),
+        port: env.int('DATABASE_PORT', 27017),
+        database: env('DATABASE_NAME', 'strapimongo'),
+        username: env('DATABASE_USERNAME', null),
+        password: env('DATABASE_PASSWORD', null),
+      },
+      options: {
+        authenticationDatabase: env('AUTHENTICATION_DATABASE', 'n'),
+        ssl: env.bool('DATABASE_SSL', false),
+      },
+    },
+  },
+});
+ 
